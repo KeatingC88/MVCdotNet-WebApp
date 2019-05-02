bd5a20d9230ed20193270523d15274900903cad4
keatingc88@gmail.com <keatingc88@gmail.com>
2019-05-02 01:17:47 -0500

    First Commit

---
+++ MVCdotNet/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCdotNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {            
+            ViewBag.List = new List<string>()
+            {
+                "One",
+                "Two",
+                "Three",
+                "Four",
+                "Five"
+            };
+
+            return View();
+        }
+
+        public string GetAssemblyVersion()
+        {
+            return typeof(Controller).Assembly.GetName().Version.ToString();
+        }
+    }
+}
